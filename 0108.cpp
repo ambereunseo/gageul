29f36c33baefc36fa8f311e2bc545e6c51e35b9d
amberchoi <afeelerchoi@gmail.com>
2025-01-08 21:42:50 +0900

    Create 0108.cpp
    swap 함수

---
+++ 0108.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void swap (int & a, int & b) {
+  int temp = a ;
+  a = b ;
+  b = temp ;
+}
+
+int main() {
+  int v1 = 5, v2 =19 ;
+  cout << "Before swap: " << "v1 = " << v1 << ", v2 = ", << v2 << "\n";
+  swap (v1,v2);
+  cout << "After swap: " << "v1 = " << v1 << ", v2 = ", << v2 << "\n";
+}
